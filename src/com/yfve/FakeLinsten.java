d7bc6dd303013590b1ee2012825776b7896ee5cc
lookflying <lookflying@gmail.com>
2013-07-23 21:29:44 +0800

    call back ok, but alloc may be wrong

--- src/com/yfve/FakeLinsten.java
+++ src/com/yfve/FakeLinsten.java
@@ -11,31 +11,38 @@ public class FakeLinsten implements VehicleTransitListen_DSP {
 	@Override
 	public void yz_3_remotedescallback(int poinum, List<POIStruct_DSP> poilist,
 			int result) {
+
 		Log.d("call back", "yz_3_remotedescallback called");
 		Log.d("call back", String.format(
 				"remotecallback poinum = %d result = %d", poinum, result));
+
 	}
 
 	@Override
 	public void yz_3_loginstatecallback(String terminalID, long userId,
 			String userName, int state) {
 		Log.d("call back", "yz_3_loginstatecallback");
-		Log.d("call back",
-				String.format(
-						"tid = %s, userId = %d, userName = %s, state = %d",
-						terminalID, userId, userName, state));
+		Log.d("call back", String.format(
+				"tid = %s, userId = %d, userName = %s, state = %d", terminalID,
+				userId, userName, state));
 
 	}
 
 	@Override
 	public void yz_3_TMCcallback(TMCStruct_DSP tmcStruct) {
+
 		Log.d("call back", "yz_3_TMCcallback called");
 
 	}
 
 	@Override
 	public void yz_3_weathercallback(WeatherStruct_DSP weatherStruct) {
-		Log.d("call back", "yz_3_weathercallback called");
+		try {
+			Log.d("call back", "yz_3_weathercallback called");
+
+		} catch (Exception e) {
+			Log.d("Exception", "in weather" + e.getMessage());
+		}
 
 	}
 
